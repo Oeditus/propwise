018aa78c4500472bda2bc41fece3e5a8cffdf43d
Aleksei Matiushkin <am@ambment.cat>
2026-01-19 11:41:42 +0100

    ✓ Prepare for hex.pm

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PropWise.MixProject do
     [
       app: :propwise,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: escript(),
